a4c0a1887e8ff8ce881ae1aef258608e64d60d9a
Moon Seongchan <boyzmsc@naver.com>
2017-09-07 14:24:06 +0900

    assignment

--- 20171620-moon seongchan-assignment2.py
+++ 20171620-moon seongchan-assignment2.py
@@ -1,10 +1,12 @@
-a = int(input("Enter a number:"))
-while a >= 0:
-    j = 1
-    for k in range (1, a+1):
-        j = j*k
-    print("%d!= %d"%(a,j))
-    a = int(input("Enter a number:"))
-if a == -1:
+try:
+    num = int(input("Enter a number:"))
+    while num >= 0:
+        result = 1
+        for k in range (1, num+1):
+            result *= k
+        print("%d!= %d"%(num,result))
+        num = int(input("Enter a number:"))
+    if num <= -1:
+        print("Error")
+except ValueError:
     print("Error")
-    
