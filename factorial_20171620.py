eb4732ef3fac263e10f194681845ad131c4ffe01
Moon Seongchan <boyzmsc@naver.com>
2017-09-25 12:28:15 +0900

    assignment

--- factorial_20171620.py
+++ factorial_20171620.py
@@ -1,13 +1,15 @@
+def Factorial(num):
+    if num == 1:
+        return num
+    elif num == 0:
+        return num+1
+    elif num < 0:
+        print("Error")
+        quit()
+    return Factorial(num-1) * num
 while True:
-    number = int(input("Enter a number: "))
-    def Factorial(num):
-        if num == 1:
-            return num
-        elif num == 0:
-            return num+1
-        elif num < 0:
-            print("Error")
-            quit()
-        return Factorial(num-1) * num
-
-    print("%d! = %d" %(number,Factorial(number)))
+    try:
+        number = int(input("Enter a number: "))
+        print("%d! = %d" %(number,Factorial(number)))
+    except ValueError:
+        print("TypeError")
