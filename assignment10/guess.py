0ffe174831aa2ffaecf035df280e23f0a3b39853
Boyzmsc <boyzmsc@naver.com>
2017-11-28 11:21:07 +0900

    assignment

--- assignment10/guess.py
+++ assignment10/guess.py
@@ -12,21 +12,19 @@ class Guess:
         return display
 
     def guess(self, character):
-        if character.isalpha():
-            if character in self.word:
-                self.guessedChars.append(character)
-                for i in range(len(self.word)):
-                    if self.word[i] == character:
-                        currentStatus_list = list(self.current_Status)
-                        currentStatus_list[i] = character
-                        self.current_Status = ''.join(currentStatus_list)
+        if character in self.word:
+            self.guessedChars.append(character)
+            for i in range(len(self.word)):
+                if self.word[i] == character:
+                    currentStatus_list = list(self.current_Status)
+                    currentStatus_list[i] = character
+                    self.current_Status = ''.join(currentStatus_list)
+        else:
+            if character in self.guessedChars:
+                pass
             else:
-                if character in self.guessedChars:
-                    pass
-                else:
-                    self.numTries += 1
-                    self.guessedChars.append(character)
-
+                self.numTries += 1
+                self.guessedChars.append(character)
 
         if self.current_Status.find('_') == -1:
             return True
